a106456c7dbbe8951ea0292cdc80e4c609c146d4
katznicho <katznicho@gmail.com>
2021-10-17 04:41:35 +0300

    implemented graphs using java

---
+++ GraphImplementation.java
@@ -0,0 +1,86 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Graph _g = new Graph(4);
+        _g.addEdge(1, 2);
+        _g.addEdge(2, 3);
+        _g.addEdge(1, 4);
+        _g.computePaths();
+
+        Scanner scanner = new Scanner(System.in);
+
+        int u, v;
+        System.out.println("Enter node1:");
+        u = scanner.nextInt();
+        System.out.println("Enter node2:");
+        v = scanner.nextInt();
+
+        if (_g.isReachable(u, v))
+            System.out.println("Yes there exists a path");
+        else
+            System.out.println("No the path doesnot exist");
+    }
+
+    // Class representing a undirected
+    // graph using matrix representation
+    static class Graph {
+        int V;
+        int[][] g;
+
+        public Graph(int V) {
+            this.V = V;
+
+            // Rows may not be contiguous
+            g = new int[V + 1][V + 1];
+            for (int i = 0; i < V + 1; i++) {
+
+                // Initialize all entries
+                // as false to indicate
+                // that there are
+                // no edges initially
+                Arrays.fill(g[i], 0);
+            }
+
+            // Initializing node to itself
+            // as it is always reachable
+            for (int i = 1; i <= V; i++)
+                g[i][i] = 1;
+        }
+
+        // Function to add edge between nodes
+        void addEdge(int v, int w) {
+            g[v][w] = 1;
+            g[w][v] = 1;
+        }
+
+        // Function to check if nodes are reachable
+        boolean isReachable(int s, int d) {
+            if (g[s][d] == 1)
+                return true;
+            else
+                return false;
+        }
+
+        // Function to compute the path
+        void computePaths() {
+
+            // Use Floyd Warshall algorithm
+            // to detect if a path exists
+            for (int k = 1; k <= V; k++) {
+
+                // Try every vertex as an
+                // intermediate vertex
+                // to check if a path exists
+                for (int i = 1; i <= V; i++) {
+                    for (int j = 1; j <= V; j++)
+                        g[i][j] = g[i][j] | ((g[i][k] != 0 && g[k][j] != 0) ? 1 : 0);
+                }
+            }
+        }
+    };
+}
